dd1c83034f5753e4e0a39c716d97deb60f8467c7
pulsar1812 <cluster000@gmail.com>
2022-08-08 21:23:40 +0800

    Initial commit

---
+++ src/utilities/formatCurrency.ts
@@ -0,0 +1,8 @@
+const currencyFormatter = new Intl.NumberFormat(undefined, {
+  currency: 'USD',
+  style: 'currency',
+})
+
+export function formatCurrency(number: number) {
+  return currencyFormatter.format(number)
+}
